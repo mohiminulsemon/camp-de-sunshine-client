a2ca5f6eec4deb0b756018d5136259bd2f74c1c4
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-06-10 01:59:09 +0600

    confirmed signup validation

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,83 +1,178 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { FcGoogle } from "react-icons/fc";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    watch, // Add watch to get the value of confirm password field
+  } = useForm();
+  const [error, setError] = useState("");
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const { createUser, updateUserProfile } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const password = watch("password", ""); // Get the value of the password field
+  const confirmPasswordErrorMessage = "Passwords do not match"; // Error message for confirm password validation
 
+  const validateConfirmPassword = (value) => {
+    if (value === password) {
+      return true;
+    }
+    return confirmPasswordErrorMessage;
+  };
+
+  const onSubmit = (data) => {
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+
+      updateUserProfile(data.name, data.photoURL)
+        .then(() => {
+          const saveUser = { name: data.name, email: data.email };
+          // fetch('https://bistro-boss-server-fawn.vercel.app/users', {
+          //     method: 'POST',
+          //     headers: {
+          //         'content-type': 'application/json'
+          //     },
+          //     body: JSON.stringify(saveUser)
+          // })
+          //     .then(res => res.json())
+          //     .then(data => {
+          //         if (data.insertedId) {
+          //             reset();
+          //             Swal.fire({
+          //                 position: 'top-end',
+          //                 icon: 'success',
+          //                 title: 'User created successfully.',
+          //                 showConfirmButton: false,
+          //                 timer: 1500
+          //             });
+          //             navigate('/');
+          //         }
+          //     })
+        })
+        .catch((error) => console.log(error));
+    });
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
         <div className="mb-8 text-center">
           <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
-          <p className="text-sm text-gray-400">Welcome to AirCNC</p>
+          <p className="text-sm text-gray-400">Welcome to Camp de Sunshine</p>
         </div>
         <form
+          onSubmit={handleSubmit(onSubmit)}
           noValidate=""
           action=""
           className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
           <div className="space-y-4">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm">
-                Name
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
               </label>
               <input
                 type="text"
+                {...register("name", { required: true })}
                 name="name"
-                id="name"
-                placeholder="Enter Your Name Here"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-                data-temp-mail-org="0"
+                placeholder="Name"
+                className="input input-bordered"
               />
+              {errors.name && (
+                <span className="text-red-600">Name is required</span>
+              )}
             </div>
-            <div>
-              <label htmlFor="image" className="block mb-2 text-sm">
-                Select Image:
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
               </label>
               <input
-                required
-                type="file"
-                id="image"
-                name="image"
-                accept="image/*"
+                type="text"
+                {...register("photoURL", { required: true })}
+                placeholder="Photo URL"
+                className="input input-bordered"
               />
+              {errors.photoURL && (
+                <span className="text-red-600">Photo URL is required</span>
+              )}
             </div>
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm">
-                Email address
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                {...register("email", { required: true })}
                 name="email"
-                id="email"
-                required
-                placeholder="Enter Your Email Here"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-                data-temp-mail-org="0"
+                placeholder="email"
+                className="input input-bordered"
               />
+              {errors.email && (
+                <span className="text-red-600">Email is required</span>
+              )}
             </div>
-            <div>
-              <div className="flex justify-between">
-                <label htmlFor="password" className="text-sm mb-2">
-                  Password
-                </label>
-              </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
               <input
                 type="password"
-                name="password"
-                id="password"
-                required
-                placeholder="*******"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                })}
+                placeholder="password"
+                className="input input-bordered"
               />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600">Password is required</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600">Password must be 6 characters</p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-600">
+                  Password must be less than 20 characters
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600">
+                  Password must have one Uppercase one lower case, one number
+                  and one special character.
+                </p>
+              )}
             </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Confirm Password</span>
+              </label>
+              <input
+                className="input input-bordered"
+                placeholder="confirm password"
+                type="password"
+                name="confirm"
+                id=""
+                required
+                {...register("confirm", {
+                  required: true,
+                  validate: validateConfirmPassword, // Add the custom validation function
+                })}
+              />
+              {errors.confirm && (
+                <p className="text-red-600">{errors.confirm.message}</p>
+              )}
+            </div>
+            <p className="text-error">{error}</p>
           </div>
 
           <div>
@@ -89,18 +184,7 @@ const SignUp = () => {
             </button>
           </div>
         </form>
-        <div className="flex items-center pt-4 space-x-1">
-          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-          <p className="px-3 text-sm dark:text-gray-400">
-            Signup with social accounts
-          </p>
-          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-        </div>
-        <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
-          <FcGoogle size={32} />
 
-          <p>Continue with Google</p>
-        </div>
         <p className="px-6 text-sm text-center text-gray-400">
           Already have an account?{" "}
           <Link
